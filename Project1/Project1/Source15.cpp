cc181c48033ae8e5cde79fb49652f9c9104d8019
IJHUnity <114376493+IJHUnity@users.noreply.github.com>
2024-10-12 11:48:07 +0900

    2024-10-12
    2024-10-12

---
+++ Project1/Project1/Source15.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+using namespace std;
+//클래스 상속
+//클래스는 객체를 이용해 계층 구조에 대한 표현이 가능합니다.
+//데이터에 대한 구조 설계에 반드시 사용되는 기법
+//공통적인 부분을 묶고 세부적인 부분을 따로 설계할 수 있습니다.
+
+
+//1. is a 상속 : ~는 ~이다로 해석이 가능한 데이터에 대한 상속
+//				상위 구조와 하위 구조를 분명하게 구별할 수 있습니다.
+//ex) 슬라임은 몬스터다.
+//class Slime : public Monster {}와 같은 방식으로 만듭니다.
+class Animal
+{
+public :
+	string name;
+	int age;
+};
+class Penguin : public Animal//Animal을 연결해서 만드는 Penguin 클래스
+{
+public :
+	void info()
+	{
+		cout << "이름 : " << name << endl; //Animal이 가지고 있는 name 사용 가능
+		cout << "나이 : " << age << endl;
+		cout << "특징 : 수영을 미친듯이 잘합니다." << endl;
+	}
+};
+class Eagle : public Animal
+{
+public:
+	void info()
+	{
+		cout << "이름 : " << name << endl;
+		cout << "나이 : " << age << endl;
+		cout << "특징 : 비행을 미친듯이 잘합니다." << endl;
+	}
+};
+//2. has A 상속 ( ~는 ~를 가지고 있다.)
+class ItemTable //아이템 테이블
+{
+public :
+	Item* item_list = NULL;
+	int item_count;
+
+	void setItem_list(int count) //개수를 넣어주면 그 개수만큼의 배열 생성
+	{
+		item_count = count;
+		item_list = new Item[item_count];
+	}
+	void setIdxItem(int idx)
+	{
+		if (idx >= 0 && idx < item_count) //0에서 item_count -1까지의 범위에서만 설정 가능
+		{
+			cout << "아이템의 이름을 입력해주세요";
+			cin >> item_list[idx].name;
+			cout << "아이템의 설명을 입력해주세요.";
+			cin >> item_list[idx].description;
+		}
+		else
+		{
+			cout << "범위를 이탈했습니다. 다시 입력을 진행해주세요 >>";
+			int new_idx;
+			cin >> new_idx;
+			setIdxItem(new_idx);
+		}
+
+
+	}
+	void showAllItemList()
+	{
+		if (item_list == NULL)
+		{
+			cout << "아이템이 없습니다." << endl;
+			return;
+		}
+		for(int i = 0; i < item_count; i++)
+		{
+			cout << "아이템 이름 : " << item_list[i].name << endl;
+			cout << item_list[i].description << endl;
+		}
+	}
+};
+class Item //아이템
+{
+public :
+	string name; //아이템 이름
+	string description; //아이템 설명
+};
+
+
+int main()
+{
+	return 0;
+}
--- Project1/Project1/data.cpp
+++ Project1/Project1/data.cpp
@@ -1,70 +1,70 @@
-#include <iostream>
-using namespace std;
-//데이터 저장 영역(컴퓨터 메모리 기준)
-//1. 데이터 영역(Data) : 전역 변수와 정적 변수가 저장되는 영역
-//2. 스택 영역(Stack) : 함수 호출 관련 데이터가 저장되는 영역(지역 변수, 함수)
-//3. 힙 영역(Heap) : 동적으로 할당한 변수와 데이터가 저장되는 영역
-
-int globalVariable = 10; //전역 변수 : 프로그램 전체에서 접근이 가능합니다.
-						 //			  프로그램 시작 시에 메모리에 할당됩니다.
-						 //			  프로그램이 종료될 때까지 유지됩니다.
-						 //프로그램 전체에서 사용하는 핵심적인 데이터를 필요할 때 사용합니다.
-
-int add(int param1, int param2) //매개 변수 
-{
-	int localVaraible = param1 + param2; //지역 변수
-	return localVaraible;
-}
-void static_func()
-{
-	static int staticVariable = 0; //정적 변수 : 프로그램이 실행될 때 메모리에 할당됩니다.
-								   //함수 내부에서 선언됩니다.(최초 1회 선언 후 그 다음부터는 선언 안함)
-								   //함수가 종료되도 메모리에 남아있는 특징을 가지고 있습니다.
-								   //-> 최초 1회 변수 생성 후, 그 변수가 유지되는 형태
-	staticVariable += 1;
-	cout << "정적 변수 : " << staticVariable << endl;
-}
-void local_func()
-{
-	int localVariable = 0;         //지역 변수 : 함수 내부에서 선언됩니다.
-								   //함수가 종료될 경우 해당 변수는 소멸됩니다.
-	localVariable += 1;
-	cout << "지역 변수 : " << localVariable << endl;
-}
-
-int main()
-{
-	int local_arg1 = 10; //지역 변수
-	int local_arg2 = 5; // 지역 변수
-	
-	int* dynamic_var = new int;
-	//동적 할당   : C언어에서는 malloc() 함수를 통해 진행
-	//             C++에서는 new 키워드를 통해 진행
-	//변수의 동적 할당 만들기
-	//자료형* 변수명 = new 자료형;
-	*dynamic_var = 10;
-	//*변수명 = 값;을 통해 동적할당한 데이터에 값 초기화
-	cout << "동적 할당 : " << *dynamic_var << endl;
-
-	cout  << "전역 변수 : " << globalVariable << endl;
-	globalVariable = 7; //전역 변수의 값을 수정할 수 있습니다.
-	cout << "전역 변수 : " << globalVariable << endl;
-	int globalVariable = 8;
-	//전역변수와 같은 이름의 지역 변수를 만들었을 경우
-	//더이상 전역변수로써의 globalVariable을 사용할 수 없습니다.
-	cout << "지역 변수 : " << globalVariable<< endl;
-
-	cout << "전역변수 함수 5회 반복" << endl;
-	for (int i = 0; i < 5; i++)
-	{
-		static_func();
-	}
-	cout << "지역변수 함수 5회 반복" << endl;
-	for (int i = 0; i < 5; i++)
-	{
-		local_func();
-	}
-
-	delete dynamic_var;
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+////데이터 저장 영역(컴퓨터 메모리 기준)
+////1. 데이터 영역(Data) : 전역 변수와 정적 변수가 저장되는 영역
+////2. 스택 영역(Stack) : 함수 호출 관련 데이터가 저장되는 영역(지역 변수, 함수)
+////3. 힙 영역(Heap) : 동적으로 할당한 변수와 데이터가 저장되는 영역
+//
+//int globalVariable = 10; //전역 변수 : 프로그램 전체에서 접근이 가능합니다.
+//						 //			  프로그램 시작 시에 메모리에 할당됩니다.
+//						 //			  프로그램이 종료될 때까지 유지됩니다.
+//						 //프로그램 전체에서 사용하는 핵심적인 데이터를 필요할 때 사용합니다.
+//
+//int add(int param1, int param2) //매개 변수 
+//{
+//	int localVaraible = param1 + param2; //지역 변수
+//	return localVaraible;
+//}
+//void static_func()
+//{
+//	static int staticVariable = 0; //정적 변수 : 프로그램이 실행될 때 메모리에 할당됩니다.
+//								   //함수 내부에서 선언됩니다.(최초 1회 선언 후 그 다음부터는 선언 안함)
+//								   //함수가 종료되도 메모리에 남아있는 특징을 가지고 있습니다.
+//								   //-> 최초 1회 변수 생성 후, 그 변수가 유지되는 형태
+//	staticVariable += 1;
+//	cout << "정적 변수 : " << staticVariable << endl;
+//}
+//void local_func()
+//{
+//	int localVariable = 0;         //지역 변수 : 함수 내부에서 선언됩니다.
+//								   //함수가 종료될 경우 해당 변수는 소멸됩니다.
+//	localVariable += 1;
+//	cout << "지역 변수 : " << localVariable << endl;
+//}
+//
+//int main()
+//{
+//	int local_arg1 = 10; //지역 변수
+//	int local_arg2 = 5; // 지역 변수
+//	
+//	int* dynamic_var = new int;
+//	//동적 할당   : C언어에서는 malloc() 함수를 통해 진행
+//	//             C++에서는 new 키워드를 통해 진행
+//	//변수의 동적 할당 만들기
+//	//자료형* 변수명 = new 자료형;
+//	*dynamic_var = 10;
+//	//*변수명 = 값;을 통해 동적할당한 데이터에 값 초기화
+//	cout << "동적 할당 : " << *dynamic_var << endl;
+//
+//	cout  << "전역 변수 : " << globalVariable << endl;
+//	globalVariable = 7; //전역 변수의 값을 수정할 수 있습니다.
+//	cout << "전역 변수 : " << globalVariable << endl;
+//	int globalVariable = 8;
+//	//전역변수와 같은 이름의 지역 변수를 만들었을 경우
+//	//더이상 전역변수로써의 globalVariable을 사용할 수 없습니다.
+//	cout << "지역 변수 : " << globalVariable<< endl;
+//
+//	cout << "전역변수 함수 5회 반복" << endl;
+//	for (int i = 0; i < 5; i++)
+//	{
+//		static_func();
+//	}
+//	cout << "지역변수 함수 5회 반복" << endl;
+//	for (int i = 0; i < 5; i++)
+//	{
+//		local_func();
+//	}
+//
+//	delete dynamic_var;
+//	return 0;
+//}
